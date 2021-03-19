6fd588ae6f98f1039f8140b46c45c94cdfd616e6
axel briano <axelbrians@gmail.com>
2021-03-19 23:33:45 +0700

    initial commit

---
+++ app/src/main/java/com/machina/google_task_clone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.machina.google_task_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
